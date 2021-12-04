1897dd7d2366e0193bd26423c2252e60b40bef99
antoninaorobets <pani.antonina.don@gmail.com>
2021-12-03 20:58:04 -0500

    add priority  selector

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   
 document.querySelector('#create-task-form').addEventListener('submit', addItem);
-
+const form = document.querySelector("#create-task-form");
 
 
 });
@@ -11,20 +11,32 @@ function addItem (evt) {
   
   const delButton = document.createElement('button')
   delButton.textContent = "x"
+  const priority = createPriority() 
 
 
-  const ToDoListItem = document.createElement('li')
-  ToDoListItem.textContent = document.querySelector('#new-task-description').value + " "
-  ToDoListItem.appendChild(delButton)
+  const toDoListItem = document.createElement('li')
+  toDoListItem.textContent = form.querySelector('#new-task-description').value + " "
+  toDoListItem.appendChild(delButton)
+  toDoListItem.appendChild(priority)
 
-  document.querySelector('#tasks').appendChild(ToDoListItem)
+  document.querySelector('#tasks').appendChild(toDoListItem)
 
   delButton.addEventListener('click', removeItem);
 
-  // evt.reset()
+  
 
 }
 
 function removeItem (evt) {
   evt.target.parentElement.remove();
 }
+
+function createPriority () {
+
+  const priority = document.createElement('select')
+  priority.innerHTML =  '<option value="high">High</option>  <option value="medium">Medium</option> <option value="low">Low</option>'
+
+return priority;
+}
+
+

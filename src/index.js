c4acad413287b0a8895e7f086fc015ddd57a45a7
antoninaorobets <pani.antonina.don@gmail.com>
2021-12-03 19:03:22 -0500

    add item to list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+document.querySelector('#create-task-form').addEventListener('submit', addItem);
+
+document.querySelector(".delete").addEventListener('click',removeItem)
+
 });
+
+function addItem (evt) {
+  evt.preventDefault()
+ 
+  const delButton = document.createElement('button')
+  delButton.textContent = "x"
+  delButton.class = "delete"
+
+  const ToDoListItem = document.createElement('li')
+  ToDoListItem.textContent = document.querySelector('#new-task-description').value + " "
+  ToDoListItem.appendChild(delButton)
+
+  document.querySelector('#tasks').appendChild(ToDoListItem)
+
+  evt.reset()
+
+}
+
+function removeItem(evt) {
+  
+
+}
